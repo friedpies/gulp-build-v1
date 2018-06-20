6c267561bd1980fbedb318825ae84f9832829398
friedpies <kenmarut@gmail.com>
2018-06-19 19:52:16 -0500

    add some gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,32 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const maps = require('gulp-sourcemaps');
 
 gulp.task("concatScripts", function(){
-  gulp.src([
+  return gulp.src([
     'js/circle/autogrow.js',
     'js/circle/circle.js',
     'js/global.js'
   ])
+  .pipe(maps.init())
   .pipe(concat("all.js"))
-  .pipe(gulp.dest("js"))
+  .pipe(maps.write("./"))
+  .pipe(gulp.dest("dist/scripts"))
 });
 
-gulp.task("minifyScripts", function(){
-  gulp.src("js/all.js")
+gulp.task("scripts", ['concatScripts'], function(){
+  gulp.src("dist/scripts/all.js")
   .pipe(uglify())
   .pipe(rename("all.min.js"))
-  .pipe(gulp.dest('js'))
+  .pipe(gulp.dest('dist/scripts'))
+});
+
+gulp.task("styles", function(){
+  gulp.src("sass/global.scss")
+  .pipe(maps.init())
+  .pipe(sass())
+  .pipe(maps.write("./"))
+  .pipe(gulp.dest("dist/styles"))
 });
